ee92369391c31ca2bd021408672b4a2bcc85f4cb
Shadorakh <shadorahgames@gmail.com>
2024-11-17 21:48:14 +0500

    Add ArrayF module with basic array operations

---
+++ Include/DOS/Collections/ArrayF.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include "DOS/Mathematics/Math.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct ArrayF
+{
+	float* data;
+	size_t size;
+} ArrayF;
+
+
+bool ArrayF_Contains(const ArrayF* const _array, const size_t _index, const float _value, const size_t _count);
+
+void ArrayF_Copy(const ArrayF* const _from, const size_t _fromIndex,
+	             ArrayF* const _to, const size_t _toIndex, const size_t _count);
+
+ArrayF* ArrayF_Create(const float _value, const size_t _size);
+
+void ArrayF_Destroy(ArrayF** _array);
+
+bool ArrayF_Equals(const ArrayF* const _left, const size_t _leftIndex, 
+	               const ArrayF* const _right, const size_t _rightIndex);
+
+void ArrayF_Fill(const ArrayF* const _array, const size_t _index, const float _value, const size_t _count);
+
+size_t ArrayF_FindIndex(const ArrayF* const _array, const size_t _index, const float _value, const size_t _count);
+
+size_t ArrayF_FindLastIndex(const ArrayF* const _array, const size_t _index, const float _value, const size_t _count);
+
+float ArrayF_Get(const ArrayF* const _array, const size_t _index);
+
+size_t ArrayF_GetSize(const ArrayF* const _array);
+
+bool ArrayF_IsNull(const ArrayF* const _array);
+
+float ArrayF_Max(const ArrayF* const _array, const size_t _index, const size_t _count);
+
+float ArrayF_Min(const ArrayF* const _array, const size_t _index, const size_t _count);
+
+void ArrayF_Reverse(const ArrayF* const _array, const size_t _index, const size_t _count);
+
+void ArrayF_Set(ArrayF* const _array, const size_t _index, const float _value);
+
+void ArrayF_ToString(char* _result, const size_t _resultSize, const ArrayF* const _array, const size_t _index);
+
+
+
+inline bool ArrayF_Equals(const ArrayF* const _left, const size_t _leftIndex,
+	                      const ArrayF* const _right, const size_t _rightIndex)
+{
+	const float left = ArrayF_Get(_left, _leftIndex);
+	const float right = ArrayF_Get(_right, _rightIndex);
+
+	return (AbsF(left) < EPSILON) && (AbsF(right) < EPSILON);
+}
+
+inline void ArrayF_Set(ArrayF* const _array, const size_t _index, const float _value)
+{
+	if (_index >= ArrayF_GetSize(_array))
+	{
+		printf("[ERROR] Out of range\n");
+		exit(EXIT_FAILURE);
+	}
+
+	_array->data[_index] = _value;
+}
+
+inline float ArrayF_Get(const ArrayF* const _array, const size_t _index)
+{
+	if (_index >= ArrayF_GetSize(_array))
+	{
+		printf("[ERROR] Out of range\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return _array->data[_index];
+}
+
+inline size_t ArrayF_GetSize(const ArrayF* const _array)
+{
+	return _array->size;
+}
+
+inline bool ArrayF_IsNull(const ArrayF* const _array)
+{
+	return (_array == NULL || _array->data == NULL);
+}
+
+inline void ArrayF_ToString(char* _result, const size_t _resultSize, const ArrayF* const _array, const size_t _index)
+{
+	snprintf(_result, _resultSize, "%.6f", ArrayF_Get(_array, _index));
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Source/DOS/Collections/ArrayF.c
@@ -0,0 +1,137 @@
+#include "DOS/Collections/ArrayF.h"
+
+#include <stdlib.h>
+
+
+bool ArrayF_Contains(const ArrayF* const _array, const size_t _index, const float _value, const size_t _count)
+{
+	for (size_t i = 0; i < _count; i++)
+	{
+		if (ArrayF_Get(_array, i + _index) == _value)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void ArrayF_Copy(const ArrayF* const _from, const size_t _fromIndex, ArrayF* const _to, const size_t _toIndex, const size_t _count)
+{
+	for (size_t i = 0; i < _count; i++)
+	{
+		ArrayF_Set(_to, i + _toIndex, ArrayF_Get(_from, i + _fromIndex));
+	}
+}
+
+ArrayF* ArrayF_Create(const float _value, const size_t _size)
+{
+	ArrayF* array = (ArrayF*)_aligned_malloc(sizeof(ArrayF), 64);
+	if (array == NULL) 
+	{
+		printf("[ERROR] Failed to allocate memory\n");
+		exit(EXIT_FAILURE);
+	}
+
+	float* block = (float*)_aligned_malloc(_size * sizeof(float), 64);
+	if (block == NULL) 
+	{
+		printf("[ERROR] Failed to allocate a block of memory\n");
+		_aligned_free(array);
+		exit(EXIT_FAILURE);
+	}
+
+	array->data = block;
+	array->size = _size;
+
+	for (size_t i = 0; i < _size; i++)
+	{
+		ArrayF_Set(array, i, _value);
+	}
+
+	return array;
+}
+
+void ArrayF_Destroy(ArrayF** _array)
+{
+	if (_array != NULL && *_array != NULL)
+	{
+		_aligned_free((*_array)->data);
+		_aligned_free(*_array);
+		(*_array)->data = NULL;
+		*_array = NULL;
+		printf("Successful memory release!\n");
+	}
+}
+
+void ArrayF_Fill(const ArrayF* const _array, const size_t _index, const float _value, const size_t _count)
+{
+	for (size_t i = 0; i < _count; i++)
+	{
+		ArrayF_Set(_array, i + _index, _value);
+	}
+}
+
+size_t ArrayF_FindIndex(const ArrayF* const _array, const size_t _index, const float _value, const size_t _count)
+{
+	for (size_t i = 0; i < _count; i++)
+	{
+		if (ArrayF_Get(_array, i + _index) == _value)
+		{
+			return i + _index;
+		}
+	}
+
+	return -1;
+}
+
+size_t ArrayF_FindLastIndex(const ArrayF* const _array, const size_t _index, const float _value, const size_t _count)
+{
+	for (size_t i = _count - 1; i > 0; i--)
+	{
+		if (ArrayF_Get(_array, i + _index) == _value)
+		{
+			return i + _index;
+		}
+	}
+
+	return -1;
+}
+
+float ArrayF_Max(const ArrayF* const _array, const size_t _index, const size_t _count)
+{
+	float max = ArrayF_Get(_array, 0);
+
+	for (size_t i = 0; i < _count; i++)
+	{
+		if (ArrayF_Get(_array, i + _index) > max)
+		{
+			max = ArrayF_Get(_array, i + _index);
+		}
+	}
+
+	return max;
+}
+
+float ArrayF_Min(const ArrayF* const _array, const size_t _index, const size_t _count)
+{
+	float min = ArrayF_Get(_array, 0);
+
+	for (size_t i = 0; i < _count; i++)
+	{
+		if (ArrayF_Get(_array, i + _index) < min)
+		{
+			min = ArrayF_Get(_array, i + _index);
+		}
+	}
+
+	return min;
+}
+
+void ArrayF_Reverse(const ArrayF* const _array, const size_t _index, const size_t _count)
+{
+	for (size_t i = 0; i < _count; i++)
+	{
+		ArrayF_Set(_array, i + _index, ArrayF_Get(_array, _count - (1 + i)));
+	}
+}
